397a60f00d9db7b27613a17263ef54dcd292b535
Gerallt Franke <geralltf@gmail.com>
2024-06-29 17:30:52 +1000

    .gitignore non-important parts.

--- CMS.Server/Controllers/ControllerActionBase.cs
+++ CMS.Server/Controllers/ControllerActionBase.cs
@@ -2,5 +2,5 @@ using Microsoft.AspNetCore.Mvc;
 
 public class ControllerActionBase : ControllerBase
 {
-    public string connectionString = "Server=.\\SQLEXPRESS2019;Database=cms;User Id=sa;Password=password;";
+    public string connectionString = "Server=.\\SQLEXPRESS2019;Database=cms;User Id=sa;Password=t0p-dog5;";
 }
